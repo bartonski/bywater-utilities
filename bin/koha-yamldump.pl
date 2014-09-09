95791d21233a22ff9b0570b0dd5d042eb2aaa114
Barton Chittenden <barton@bywatersolutions.com>
2014-09-09 10:49:10 -0400

    Replace hard coded query with STDIN or file.

--- bin/koha-yamldump.pl
+++ bin/koha-yamldump.pl
@@ -5,17 +5,8 @@ use warnings;
 use C4::Context;
 use YAML;
 
-my $statement = 'select * 
-from 
-    reserves
-    inner join items using (itemnumber)
-    inner join biblio using (biblionumber)
-    inner join biblioitems using (biblionumber)
-    inner join borrowers using (borrowernumber)
-where
-    items.barcode = '31562015171368'
-';
-my $key_field = qw( reserves.reservenumber, items.itemnumber, biblio.biblionumber, biblioitems.biblioitemnumber, borrowers.borrowrnumber );
-my $output=C4::Context->{dbh}->selectall_hashref($statement, $key_field);
+my @query = <>;
+my $query = join( "", @query );
+my $output=C4::Context->{dbh}->$dbh->selectall_arrayref($query, { Slice => {} });
 
 print YAML::Dumper( $output );
