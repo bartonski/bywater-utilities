1367f090ae2f7fb99f2a69c47512116889d8670e
Barton Chittenden <barton@bywatersolutions.com>
2015-01-31 19:50:40 -0500

    koha-print-circ-rule.pl: change function print_header() header(), add $borrowerinfo and $branchitemrule

--- bin/koha-print-circ-rule.pl
+++ bin/koha-print-circ-rule.pl
@@ -10,12 +10,12 @@ use C4::Biblio;
 use Getopt::Long;
 use Data::Dumper;
 use C4::Context;
-sub print_header {
+sub header {
     my $string = "@_";
     my $underline = '=' x length( $string );
-    print "\n";
-    print "${string}\n";
-    print "${underline}\n";
+    return   "\n"
+           . "${string}\n"
+           . "${underline}\n";
 }
 
 my $opt_borrower_card_num;
@@ -27,16 +27,19 @@ GetOptions(
 
 my $item = GetItem(GetItemnumberFromBarcode( $opt_item_barcode ));
 my ($borrower) = &GetMemberDetails( '', $opt_borrower_card_num); # not sure why this was returned in list context, but it's that way in the perldoc.
+my $borrowerinfo = C4::Members::GetMember( borrowernumber => $borrower->{borrowernumber} );
 my $branch = C4::Circulation::_GetCircControlBranch($item,$borrower);
 my $borrowertype = $borrower->{categorycode};
 my $biblioitem = GetBiblioItemData($item->{biblioitemnumber});
 my $itemtype = ( C4::Context->preference('item-level_itypes') ) ? $item->{'itype'} : $biblioitem->{'itemtype'};
 my $loanlength = C4::Circulation::GetLoanLength($borrowertype,$itemtype,$branch);
 my ($hardduedate,$hardduedatecompare) = C4::Circulation::GetHardDueDate($borrowertype,$itemtype,$branch);
+my $branchitemrule = C4::Circulation::GetBranchItemRule( $branch, $item->{'itype'} );
 my $irule = C4::Circulation::GetIssuingRule($borrowertype,$itemtype,$branch);
 
 my @fields = (
       [ 'Borrower'                 => $borrower           ]
+    , [ 'Borrower Info'            => $borrowerinfo       ]
     , [ 'Item'                     => $item               ]
     , [ 'Circulation Branch'       => $branch             ]
     , [ 'Item type'                => $itemtype           ]
@@ -44,10 +47,11 @@ my @fields = (
     , [ 'Hard Due Date'            => $hardduedate        ]
     , [ 'Hard Due Date Comparison' => $hardduedatecompare ]
     , [ 'Issuing Rule'             => $irule              ]
+    , [ 'Branch Item Rule'         => $branchitemrule     ]
 );
 
 for my $field ( @fields ) {
     my ( $header, $value ) = @$field;
-    print_header( $header );
+    print header( $header );
     print Dumper( $value );
 }
