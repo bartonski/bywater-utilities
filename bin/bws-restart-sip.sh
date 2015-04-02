103b7394d9c9f075e3708265f4090a8666343279
Barton Chittenden <barton@bywatersolutions.com>
2015-04-02 09:19:49 -0400

    Fix comments, change sleep times to 5.

--- bin/bws-restart-sip.sh
+++ bin/bws-restart-sip.sh
@@ -8,9 +8,10 @@ PATH="$PATH:/usr/sbin:/usr/bin"
 # The script will do the following:
 #
 # Request a graceful shutdown of all running SIP processes
-# Sleep for 10 seconds to allow processes to time to end
+# Sleep for 5 seconds to allow processes to time to end
 # Run pgrep to check for running SIP processes
 # Kill any SIP processes which did not shut down gracefully
+# Sleep for 5 seconds to allow processes to time to end
 # Restart SIP
 
 # This script should be run either as the koha user for git sites
@@ -31,10 +32,10 @@ check_dependency /usr/bin/pgrep
 check_dependency /usr/bin/pkill
 
 if [ -e /usr/sbin/koha-list ]
-then # Set up for git sites
+then # Set up for package sites
     STOPCMD='for instance in $(sudo koha-list --enabled); do sudo koha-stop-sip $instance 2>&1 > /dev/null; done'
     STARTCMD='for instance in $(sudo koha-list --enabled); do sudo koha-start-sip $instance 2>&1 > /dev/null; done'
-else # Set up for package sites
+else # Set up for git sites
     check_dependency "$HOME/sip-daemon.sh"
     STOPCMD="sudo $HOME/sip-daemon.sh stop 2>&1 > /dev/null"
     STARTCMD="sudo $HOME/sip-daemon.sh start 2>&1 > /dev/null"
@@ -45,14 +46,14 @@ fi
 
 eval "$STOPCMD"
 
-sleep 10;
+sleep 5;
 
 for pid in $(pgrep -f SIP)
 do
     sudo kill $pid || sudo kill -9 $pid
 done
 
-sleep 10;
+sleep 5;
 
 if process="$(pgrep -f SIP)"
 then
